ecdcaa89a648cf96e7178341ee3bd5bd5f4bbd53
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-21 10:32:01 +0530

    Snake and Ladder

---
+++ ReviewPrgrams/ReviewPrgrams/SnakeLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewPrgrams
+{
+    internal class SnakeLadder
+    {
+        public static void Main(string[] args)
+        {
+            List<int> Snakes = new List<int> { 3, 14, 23, 41 };
+
+            List<int> Ladder = new List<int> { 9, 18, 37, 29 };
+
+
+            int Start = 0;
+            Random Dice = new Random();
+
+        }
+
+    }
+}

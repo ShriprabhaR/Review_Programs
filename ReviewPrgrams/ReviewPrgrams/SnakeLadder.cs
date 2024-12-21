a330839c57f8577fcd9cbff4e34d6fed47505994
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-21 11:59:27 +0530

    UC3

--- ReviewPrgrams/ReviewPrgrams/SnakeLadder.cs
+++ ReviewPrgrams/ReviewPrgrams/SnakeLadder.cs
@@ -10,13 +10,34 @@ namespace ReviewPrgrams
         {
             int StartPos = 0;
             Console.WriteLine("One person started the game from position 0");
-
-           
         }
-        public static void DiceRoll()
+
+        public static void Game()
         {
+
             Random Dice = new Random();
-            Dice.Next(1, 7);
+            int DiceValue = Dice.Next(1, 7);
+
+            Console.WriteLine("Dice Rolled "+DiceValue);
+
+            int PersonPos = 1;
+
+            if (DiceValue == 6 || DiceValue == 4)
+            {
+                Console.WriteLine("No movement for user");
+            }
+
+            else if (DiceValue == 2 || DiceValue == 5)
+            {
+               PersonPos = PersonPos - DiceValue; 
+               Console.WriteLine("Skake bites");
+            }
+            else
+            { 
+               PersonPos = PersonPos + DiceValue;
+               Console.WriteLine("Climbed Ladder");
+            }
+
         }
 
     }
